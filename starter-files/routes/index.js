073cea2d191370f3aac689deacf9c98d366c98d4
Juan Pablo Almonacid <jpalmonacidv@gmail.com>
2019-05-30 16:29:34 -0300

    Video #5 Templating

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -6,7 +6,10 @@ router.get('/', (req, res) => {
   const wes = { name: 'Wes', age: 100, cool: true };
   // res.send('Hey! It works!');
   // res.json(wes);
-  res.send(req.query.name);
+  // res.send(req.query.name);
+  res.render('hello', {
+    name: req.query.name
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
