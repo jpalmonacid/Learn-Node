b3f33253581ca4a3b91a8d04e40601b58e95fefb
Juan Pablo Almonacid <jpalmonacidv@gmail.com>
2019-06-07 15:15:25 -0300

    Video #12 Flash messages

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -13,5 +13,6 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = new Store(req.body);
   await store.save();
-  res.redirect('/');
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
